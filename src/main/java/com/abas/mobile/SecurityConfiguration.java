93ef07aade07b50d78d4583b2ab207b187fe413e
Metehan <metehanmeral@hotmail.com>
2019-05-05 01:37:01 +0300

    20190502

--- src/main/java/com/abas/mobile/SecurityConfiguration.java
+++ src/main/java/com/abas/mobile/SecurityConfiguration.java
@@ -3,15 +3,45 @@ package com.abas.mobile;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
 	
+	// @Autowired
+	// private SessionRegistry sessionRegistry;
+	//
+	// @Autowired
+	// private com.abas.mobile.SessionListener sessionListener;
+	
 	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception
+	protected void configure(HttpSecurity http) throws Exception
 	{
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests()
+		    .antMatchers("/")
+		    .permitAll();
+		http.sessionManagement()		   
+		    .maximumSessions(100)
+		    .maxSessionsPreventsLogin(false)
+		    .and()
+		    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+		    .sessionFixation().migrateSession();
+		// .sessionRegistry(sessionRegistry());
+		
 	}
+	
+	// @Bean
+	// public ServletListenerRegistrationBean<HttpSessionListener> sessionListener1()
+	// {
+	// return new ServletListenerRegistrationBean<HttpSessionListener>(new SessionListener());
+	// }
+	
+	//
+	// @Bean
+	// public SessionRegistry sessionRegistry()
+	// {
+	// return new SessionRegistryImpl();
+	// }
 	
 }
--- src/main/java/com/abas/mobile/web/WebLinkController.java
+++ src/main/java/com/abas/mobile/web/WebLinkController.java
@@ -1,19 +1,68 @@
 package com.abas.mobile.web;
 
-import org.springframework.context.i18n.LocaleContextHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.abas.mobile.SprinBootAppConfiguration;
+import com.abas.mobile.service.SessionRegistry;
 
 @Controller
 public class WebLinkController
 {
+	@Autowired
+	private SessionRegistry sessionRegistry;
+	
+	Logger LOGGER=LoggerFactory.getLogger(SprinBootAppConfiguration.class);
+	
 	@RequestMapping(value=
-	{"/abasadmin/actuators"})
-	public ModelAndView indexTh()
+	{"/abasadmin"})
+	public ModelAndView abasadmin()
 	{
 		ModelAndView mav=new ModelAndView();
-		mav.setViewName("th_actuators");
+		mav.setViewName("th_abasadmin");
+		return mav;
+	}
+	
+	@RequestMapping(value=
+	{"/abasadmin/actuators"})
+	public ModelAndView actuators()
+	{
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("th_abasadmin_actuators");
+		return mav;
+	}
+	
+	@RequestMapping(value=
+	{"/abasadmin/activesessions"})
+	public ModelAndView activesessions()
+	{
+		ModelAndView mav=new ModelAndView();
+		LOGGER.info(""+SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		LOGGER.info(""+SecurityContextHolder.getContext().getAuthentication().getName());
+		LOGGER.info(""+SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+		LOGGER.info(""+SecurityContextHolder.getContext().getAuthentication().getCredentials().toString());
+		LOGGER.info(""+SecurityContextHolder.getContext().getAuthentication().getDetails().toString());
+		LOGGER.info(""+sessionRegistry.getSessions().size());
+		
+		// LOGGER.info(""+sessionRegistry.getAllSessions(SecurityContextHolder.getContext().getAuthentication().getPrincipal(),false).size());
+		//
+		// LOGGER.info(""+sessionRegistry.getAllPrincipals().size());
+		// LOGGER.info(""+sessionRegistry.getAllSessions(SecurityContextHolder.getContext().getAuthentication().getPrincipal(),false).size());
+		
+		// UserDetails currenstsession=(UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		//
+		// List<UserDetails> activesessions=sessionRegistry.getAllPrincipals().stream()
+		// .filter(u->!sessionRegistry.getAllSessions(u,true).isEmpty())
+		// .map(u->(UserDetails)u)
+		// .collect(Collectors.toList());
+		//
+		// mav.addObject("activeSessions",activesessions);
+		// mav.addObject("currenstSession",currenstsession);
+		mav.setViewName("th_abasadmin_activesessions");
 		return mav;
 	}
 }
